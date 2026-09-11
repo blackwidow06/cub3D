e4305fb0e67586f6de8289e8689150f8a046ca1a
Marco Rojouan <marcoprojouan@outlook.fr>
2026-09-11 11:49:39 +0200

    youpi

--- display.c
+++ display.c
@@ -3,14 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   display.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malavaud <malavaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 10:49:05 by malavaud          #+#    #+#             */
-/*   Updated: 2026/09/11 11:04:15 by malavaud         ###   ########.fr       */
+/*   Updated: 2026/09/11 11:27:46 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+int	exit_window(t_game *game)
+{
+	if (game->map)
+		free_tab(game->map);
+	if (game->mlx)
+	{
+		if (game->floor)
+			mlx_destroy_image(game->mlx, game->floor);
+		if (game->wall)
+			mlx_destroy_image(game->mlx, game->wall);
+		if (game->player)
+			mlx_destroy_image(game->mlx, game->player);
+		if (game->collect)
+			mlx_destroy_image(game->mlx, game->collect);
+		if (game->exit)
+			mlx_destroy_image(game->mlx, game->exit);
+		if (game->window)
+			mlx_destroy_window(game->mlx, game->window);
+		mlx_destroy_display(game->mlx);
+		gnl_clear(game);
+		free(game->mlx);
+	}
+	exit(1);
+}
 
 int	main(void)
 {
