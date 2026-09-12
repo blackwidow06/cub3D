906a610808eeb585a55b1fe46a4cfb1127c35c3f
Maeva Lavaud <malavaud@1A1.42angouleme.fr>
2026-09-12 10:32:27 +0200

    feat:libft/struct

---
+++ libft/ft_calloc.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malavaud <malavaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 10:11:29 by malavaud          #+#    #+#             */
+/*   Updated: 2025/10/23 17:36:56 by malavaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*tmp;
+	size_t			i;
+
+	i = 0;
+	if (count != 0 && count * size / count != size)
+		return (NULL);
+	if (!count || !size)
+		return (malloc(0));
+	tmp = malloc(count * size);
+	if (!tmp)
+		return (NULL);
+	while (i < count * size)
+		tmp[i++] = 0;
+	return ((void *)tmp);
+}
---
+++ libft/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malavaud <malavaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 15:15:45 by malavaud          #+#    #+#             */
+/*   Updated: 2025/10/22 13:33:49 by malavaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	d;
+	size_t	s;
+	size_t	i;
+
+	d = 0;
+	s = 0;
+	if (!dest && size == 0)
+		return (ft_strlen(src));
+	while (dest[d] && d < size)
+		d++;
+	while (src[s])
+		s++;
+	i = 0;
+	while (src[i] && (d + i + 1) < size)
+	{
+		dest[d + i] = src[i];
+		i++;
+	}
+	if (d < size)
+		dest[d + i] = '\0';
+	return (d + s);
+}
