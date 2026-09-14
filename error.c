39214e9f948482f2ad3aae2139b757dff854c298
Maeva Lavaud <malavaud@1A1.42angouleme.fr>
2026-09-14 11:27:01 +0200

    fvdfzfsd

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: malavaud <malavaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 10:15:34 by malavaud          #+#    #+#             */
-/*   Updated: 2026/09/14 10:16:49 by malavaud         ###   ########.fr       */
+/*   Updated: 2026/09/14 10:39:13 by malavaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 int	exit_game(t_game *game, char *end_mes)
 {
-	mlx_destroy_display(game->mlx);
-	free(game->mlx);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);	
+	}
 	if (end_mes)
 		printf("%s", end_mes);
 	exit(0);
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: malavaud <malavaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/14 10:11:43 by malavaud          #+#    #+#             */
-/*   Updated: 2026/09/14 10:12:16 by malavaud         ###   ########.fr       */
+/*   Updated: 2026/09/14 11:10:38 by malavaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	parsing(char *filename, t_texture *texture, t_game *game)
 {
 	if (check_map_file(filename) != 0)
-		exit_game(game, "Error\n file .cub invalid\n");
+		exit_game(game, "Error\nfile .cub invalid\n");
 	if (read_map(filename, texture) != 0)
-		exit_game(game, "Error\n Invalid read map\n");
+		exit_game(game, NULL);
 	if (check_textures(texture) != 0)
-		exit_game(game, "Error\n Invalid texture\n");
+		exit_game(game, "Error\nInvalid texture\n");
 	return (0);
 }
